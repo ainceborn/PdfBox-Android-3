ca742d3d107f9d95b160e4c5dd8aea636b0700d6
Kanstantsin Valeitsenak <kanstantsin.valeitsenak@sap.com>
2026-07-14 20:19:15 +0200

    fix tests

--- library/src/test/java/com/ainceborn/pdfbox/pdmodel/interactive/form/TestListBox.java
+++ library/src/test/java/com/ainceborn/pdfbox/pdmodel/interactive/form/TestListBox.java
@@ -16,6 +16,11 @@
  */
 package com.ainceborn.pdfbox.pdmodel.interactive.form;
 
+import static com.ainceborn.pdfbox.pdmodel.font.Standard14Fonts.FontName.*;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThrows;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -23,11 +28,21 @@ import java.util.List;
 
 import com.ainceborn.pdfbox.cos.COSArray;
 import com.ainceborn.pdfbox.cos.COSName;
+import com.ainceborn.pdfbox.io.IOUtils;
 import com.ainceborn.pdfbox.pdmodel.PDDocument;
+import com.ainceborn.pdfbox.pdmodel.PDPage;
+import com.ainceborn.pdfbox.pdmodel.PDResources;
+import com.ainceborn.pdfbox.pdmodel.common.PDRectangle;
+import com.ainceborn.pdfbox.pdmodel.font.PDFont;
+import com.ainceborn.pdfbox.pdmodel.font.PDType1Font;
+import com.ainceborn.pdfbox.pdmodel.interactive.annotation.PDAnnotationWidget;
 
-import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 /**
  * This will test the functionality of choice fields in PDFBox.
@@ -35,203 +50,219 @@ import junit.framework.TestSuite;
 public class TestListBox extends TestCase
 {
 
-    /**
-     * Constructor.
-     *
-     * @param name The name of the test to run.
-     */
-    public TestListBox( String name )
+    private List<String> exportValues;
+    private List<String> displayValues;
+    private PDDocument doc;
+    PDListBox choice;
+
+    @Before
+    public void setUp()
     {
-        super( name );
-    }
-
-    /**
-     * This will get the suite of test that this class holds.
-     *
-     * @return All of the tests that this class holds.
-     */
-    public static Test suite()
-    {
-        return new TestSuite( TestListBox.class );
-    }
-
-    /**
-     * infamous main method.
-     *
-     * @param args The command line arguments.
-     */
-    public static void main( String[] args )
-    {
-        String[] arg = {TestListBox.class.getName() };
-        junit.textui.TestRunner.main( arg );
-    }
-
-    /**
-     * This will test the list box PDModel.
-     *
-     * @throws IOException If there is an error creating the field.
-     */
-    public void testListboxPDModel() throws IOException
-    {
-
-        /*
-         * Set up two data lists which will be used for the tests
-         */
-
         // export values
-        List<String> exportValues = new ArrayList<String>();
+        exportValues = new ArrayList<>();
         exportValues.add("export01");
         exportValues.add("export02");
         exportValues.add("export03");
 
         // display values, not sorted on purpose as this
         // will be used to test the sort option of the list box
-        List<String> displayValues = new ArrayList<String>();
+        displayValues = new ArrayList<>();
         displayValues.add("display02");
         displayValues.add("display01");
         displayValues.add("display03");
 
-        PDDocument doc = null;
-        try
-        {
-            doc = new PDDocument();
-            PDAcroForm form = new PDAcroForm( doc );
-            PDListBox choice = new PDListBox(form);
+        doc = new PDDocument();
+        PDPage page = new PDPage(PDRectangle.A4);
+        doc.addPage(page);
+        PDAcroForm form = new PDAcroForm( doc );
 
-            // appearance construction is not implemented, so turn on NeedAppearances
-            form.setNeedAppearances(true);
+        // Adobe Acrobat uses Helvetica as a default font and
+        // stores that under the name '/Helv' in the resources dictionary
+        PDFont font = new PDType1Font(HELVETICA);
+        PDResources resources = new PDResources();
+        resources.put(COSName.HELV, font);
 
-            // test that there are no nulls returned for an empty field
-            // only specific methods are tested here
-            assertNotNull(choice.getOptions());
-            assertNotNull(choice.getValue());
+        // Add and set the resources and default appearance at the form level
+        form.setDefaultResources(resources);
 
-            /*
-             * Tests for setting the export values
-             */
+        // Acrobat sets the font size on the form level to be
+        // auto sized as default. This is done by setting the font size to '0'
+        String defaultAppearanceString = "/Helv 0 Tf 0 g";
+        form.setDefaultAppearance(defaultAppearanceString);
 
-            // setting/getting option values - the dictionaries Opt entry
-            choice.setOptions(exportValues);
-            assertEquals(exportValues,choice.getOptionsDisplayValues());
-            assertEquals(exportValues,choice.getOptionsExportValues());
+        // the choice field for testing
+        choice = new PDListBox(form);
 
-            // Test bug 1 of PDFBOX-4252 when top index is not null
-            choice.setTopIndex(1);
-            choice.setValue(exportValues.get(2));
-            assertEquals(exportValues.get(2), choice.getValue().get(0));
-            choice.setTopIndex(null); // reset
+        choice.setDefaultAppearance("/Helv 12 Tf 0g");
 
-            // assert that the option values have been correctly set
-            COSArray optItem = (COSArray) choice.getCOSObject().getItem(COSName.OPT);
-            assertNotNull(choice.getCOSObject().getItem(COSName.OPT));
-            assertEquals(optItem.size(),exportValues.size());
-            assertEquals(exportValues.get(0), optItem.getString(0));
 
-            // assert that the option values can be retrieved correctly
-            List<String> retrievedOptions = choice.getOptions();
-            assertEquals(retrievedOptions.size(),exportValues.size());
-            assertEquals(retrievedOptions, exportValues);
+        // Specify the annotation associated with the field
+        PDAnnotationWidget widget = choice.getWidgets().get(0);
+        PDRectangle rect = new PDRectangle(50, 750, 200, 50);
+        widget.setRectangle(rect);
+        widget.setPage(page);
 
-            /*
-             * Tests for setting the field values
-             */
+        // Add the annotation to the page
+        page.getAnnotations().add(widget);
+    }
 
-            // assert that the field value can be set
-            choice.setValue("export01");
-            assertEquals(choice.getValue().get(0),"export01");
+    @Test
+    public void testNoNullsReturned()
+    {
+        // test that there are no nulls returned for an empty field
+        // only specific methods are tested here
+        assertNotNull(choice.getOptions());
+        assertNotNull(choice.getValue());
+    }
 
-            // ensure that the choice field doesn't allow multiple selections
-            choice.setMultiSelect(false);
+    /*
+     * Tests for setting the export values
+     */
+    @Test
+    public void testExportValuesGetterSetter() throws IOException
+    {
+        // setting/getting option values - the dictionaries Opt entry
+        choice.setOptions(exportValues);
+        assertEquals(exportValues,choice.getOptionsDisplayValues());
+        assertEquals(exportValues,choice.getOptionsExportValues());
 
-            // without multiselect setting multiple items shall fail
-            try
-            {
-                choice.setValue(exportValues);
-                fail( "Missing IllegalArgumentException" );
-            }
-            catch( IllegalArgumentException e )
-            {
-                assertEquals("The list box does not allow multiple selections.",e.getMessage() );
-            }
+        // Test bug 1 of PDFBOX-4252 when top index is not null
+        choice.setTopIndex(1);
+        choice.setValue(exportValues.get(2));
+        assertEquals(exportValues.get(2), choice.getValue().get(0));
+        choice.setTopIndex(null); // reset
 
-            // ensure that the choice field does allow multiple selections
-            choice.setMultiSelect(true);
-            // now this call must succeed
+        // assert that the option values have been correctly set
+        COSArray optItem = (COSArray) choice.getCOSObject().getItem(COSName.OPT);
+        assertNotNull(choice.getCOSObject().getItem(COSName.OPT));
+        assertEquals(optItem.size(),exportValues.size());
+        assertEquals(exportValues.get(0), optItem.getString(0));
+
+        // assert that the option values can be retrieved correctly
+        List<String> retrievedOptions = choice.getOptions();
+        assertEquals(retrievedOptions.size(),exportValues.size());
+        assertEquals(retrievedOptions, exportValues);
+        // assert that the field value can be set
+    }
+
+    /*
+     * Test for setting the field value
+     */
+    @Test
+    public void testFieldValueSetterGetter() throws IOException
+    {
+        // add test data
+        choice.setOptions(exportValues);
+        choice.setMultiSelect(true);
+        choice.setValue(exportValues);
+
+        // assert that the option values have been correctly set
+        COSArray valueItems = (COSArray) choice.getCOSObject().getItem(COSName.V);
+        assertNotNull(valueItems);
+        assertEquals(valueItems.size(),exportValues.size());
+        assertEquals(exportValues.get(0), valueItems.getString(0));
+
+        // assert that the index values have been correctly set
+        COSArray indexItems = (COSArray) choice.getCOSObject().getItem(COSName.I);
+        assertNotNull(indexItems);
+        assertEquals(indexItems.size(),exportValues.size());
+
+        // setting a single value shall remove the indices
+        choice.setValue("export01");
+        indexItems = (COSArray) choice.getCOSObject().getItem(COSName.I);
+        assertNull(indexItems);
+    }
+
+    @Test
+    public void testMultiselect() throws IOException
+    {
+        // add test data
+        choice.setOptions(exportValues);
+
+        // ensure that the choice field doesn't allow multiple selections
+        choice.setMultiSelect(false);
+
+        // without multiselect setting multiple items shall fail
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
             choice.setValue(exportValues);
+        });
 
-            // assert that the option values have been correctly set
-            COSArray valueItems = (COSArray) choice.getCOSObject().getItem(COSName.V);
-            assertNotNull(valueItems);
-            assertEquals(valueItems.size(),exportValues.size());
-            assertEquals(exportValues.get(0), valueItems.getString(0));
+        assertEquals("The list box does not allow multiple selections.", exception.getMessage());
 
-            // assert that the index values have been correctly set
-            COSArray indexItems = (COSArray) choice.getCOSObject().getItem(COSName.I);
-            assertNotNull(indexItems);
-            assertEquals(indexItems.size(),exportValues.size());
+        // ensure that the choice field does allow multiple selections
+        choice.setMultiSelect(true);
+        // now this call must succeed
+        choice.setValue(exportValues);
+    }
 
-            // setting a single value shall remove the indices
-            choice.setValue("export01");
-            indexItems = (COSArray) choice.getCOSObject().getItem(COSName.I);
-            assertNull(indexItems);
+    @Test
+    public void testOptIsRemovedForNull()
+    {
+        // add test data
+        choice.setOptions(exportValues);
+        assertNotNull(choice.getCOSObject().getItem(COSName.OPT));
+        // assert that the Opt entry is removed
+        choice.setOptions(null);
+        assertNull(choice.getCOSObject().getItem(COSName.OPT));
+        // if there is no Opt entry an empty List shall be returned
+        assertEquals(Collections.emptyList(), choice.getOptions());
+    }
 
-            // assert that the Opt entry is removed
-            choice.setOptions(null);
-            assertNull(choice.getCOSObject().getItem(COSName.OPT));
-            // if there is no Opt entry an empty List shall be returned
-            assertEquals(choice.getOptions(), Collections.<String>emptyList());
+    @Test
+    public void testSetExportAndDisplay()
+    {
+        // setting display and export value
+        choice.setOptions(exportValues, displayValues);
+        assertEquals(displayValues,choice.getOptionsDisplayValues());
+        assertEquals(exportValues,choice.getOptionsExportValues());
+    }
 
-            /*
-             * Test for setting export and display values
-             */
+    @Test
+    public void testSortOption()
+    {
+        // add test data
+        choice.setOptions(exportValues, displayValues);
+        assertEquals("display02", choice.getOptionsDisplayValues().get(0));
 
-            // setting display and export value
+        // test the sort option
+        choice.setSort(true);
+        choice.setOptions(exportValues, displayValues);
+        assertEquals("display01", choice.getOptionsDisplayValues().get(0));
+        assertEquals("display02", choice.getOptionsDisplayValues().get(1));
+        assertEquals("display03", choice.getOptionsDisplayValues().get(2));
+    }
+
+    @Test
+    public void testEmptyOptionsNotNull()
+    {
+        // assert that the Opt entry is removed
+        choice.setOptions(null, displayValues);
+        assertNull(choice.getCOSObject().getItem(COSName.OPT));
+
+        // if there is no Opt entry an empty list shall be returned
+        assertEquals(Collections.emptyList(), choice.getOptions());
+        assertEquals(Collections.emptyList(), choice.getOptionsDisplayValues());
+        assertEquals(Collections.emptyList(), choice.getOptionsExportValues());
+    }
+
+    @Test
+    public void testExceptionForDifferentNumberOfEntries()
+    {
+        // test that an IllegalArgumentException is thrown when export and display
+        // value lists have different sizes
+        exportValues.remove(1);
+
+        // without multiselect setting multiple items shall fail
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
             choice.setOptions(exportValues, displayValues);
-            assertEquals(displayValues,choice.getOptionsDisplayValues());
-            assertEquals(exportValues,choice.getOptionsExportValues());
+        });
 
-            /*
-             * Testing the sort option
-             */
-            assertEquals(choice.getOptionsDisplayValues().get(0),"display02");
-            choice.setSort(true);
-            choice.setOptions(exportValues, displayValues);
-            assertEquals(choice.getOptionsDisplayValues().get(0),"display01");
+        assertEquals("The number of entries for exportValue and displayValue shall be the same.", exception.getMessage());
+    }
 
-            /*
-             * Setting options with an empty list
-             */
-            // assert that the Opt entry is removed
-            choice.setOptions(null, displayValues);
-            assertNull(choice.getCOSObject().getItem(COSName.OPT));
-
-            // if there is no Opt entry an empty list shall be returned
-            assertEquals(choice.getOptions(), Collections.<String>emptyList());
-            assertEquals(choice.getOptionsDisplayValues(), Collections.<String>emptyList());
-            assertEquals(choice.getOptionsExportValues(), Collections.<String>emptyList());
-
-            // test that an IllegalArgumentException is thrown when export and display 
-            // value lists have different sizes
-            exportValues.remove(1);
-
-            try
-            {
-                choice.setOptions(exportValues, displayValues);
-                fail( "Missing exception" );
-            }
-            catch( IllegalArgumentException e )
-            {
-                assertEquals(
-                    "The number of entries for exportValue and displayValue shall be the same.",
-                    e.getMessage() );
-            }
-        }
-        finally
-        {
-            if( doc != null )
-            {
-                doc.close();
-            }
-        }
+    @After
+    public void tearDown()
+    {
+        IOUtils.closeQuietly(doc);
     }
 }
